04575d08607fa4eb2322038110cc616d52c4017e
cb-abd-dev <cb@abd.dev>
2023-02-28 06:27:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sweet-tooth-1",
+  url: "https://sweet-tooth-1.botics.co",
+  api: "https://sweet-tooth-1.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
